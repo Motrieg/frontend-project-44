58977f4064ba13411bb507fc1e3148c62df6fe4b
Михаил Савельев <motriegs@gmail.com>
2023-07-01 12:55:42 +0300

    Revert "CC changes check"
    This reverts commit b4481a512dea0c20fc9cf22d6173dcfe4326a5ea.

--- games/brain-even.js
+++ games/brain-even.js
@@ -4,7 +4,7 @@ const isEvenGame = () => {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
   for (let i = 0; i < 3;) {
-    const randomNumber = Math.floor((Math.random() * 100) + 1);
+    const randomNumber = logic.getRandomNumber();
     console.log(`Question: ${randomNumber}`);
     const answer = logic.answerPromt();
     const correctAnswer = (randomNumber % 2) === 0 ? 'yes' : 'no';
