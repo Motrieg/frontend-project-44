28a9d1f5a02f4706e3e67baab70c9df168271109
Михаил Савельев <motriegs@gmail.com>
2023-07-01 18:13:08 +0300

    test fix

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readLineSync from 'readline-sync';
 
 const greeting = () => {
   console.log('Welcome to the Brain Games!');
-  const name = readLineSync.question('May i have your name?');
+  const name = readLineSync.question('May i have your name? ');
   console.log(`Hello, ${name}!`);
 };
 
